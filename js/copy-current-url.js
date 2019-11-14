dbbe5c48e725b2d8701840c197ee0aeb126e7fc7
Joery Lemmens <joery.lemmens@brandle.be>
2019-11-14 11:25:56 +0100

    do not jump to page end when copying link

--- js/copy-current-url.js
+++ js/copy-current-url.js
@@ -1,76 +1,77 @@
 /**
  * Copy the current page url to clipboard when clicking on the .btnCopy button.
- * Based on https://stackoverflow.com/questions/400212/how-do-i-copy-to-the-clipboard-in-javascript/30810322#30810322
+ * Based on
+ * https://stackoverflow.com/questions/400212/how-do-i-copy-to-the-clipboard-in-javascript/30810322#30810322
  */
 
-// Selector of the element that copies link when clicked.
-var copyButtonElements = document.querySelectorAll('.btnCopy');
-
-// Add click event listener to the button(s).
-copyButtonElements.forEach(function (element) {
+(function ($) {
   'use strict';
-  // Adding click event on each anchor element.
-  element.addEventListener('click', function (e) {
-    var popupElements = document.querySelectorAll('.social-sharing-buttons__popup');
-    copyTextToClipboard(window.location.href, popupElements);
-  });
-});
 
-/*
- * Function to copy current url to clipboard. Shows a popupmessage on screen if url was copied successful.
- */
-function copyTextToClipboard(text, popupElements) {
-  'use strict';
-  if (!navigator.clipboard) {
-    fallbackCopyTextToClipboard(text, popupElements);
-    return;
-  }
+  // Selector of the element that copies link when clicked.
+  Drupal.behaviors.copyButtonElements = {
+    attach: function (context) {
+      var $copyButton = $('.btnCopy');
+      $copyButton.once('copy-current-url').each(function () {
+        // Adding click event on each anchor element.
+        $(this).on('click', function (e) {
+          e.preventDefault();
+          var popupElements = $('.social-sharing-buttons__popup');
+          Drupal.copyTextToClipboard(window.location.href, popupElements);
+        });
+      });
+    }
+  };
 
-  navigator.clipboard.writeText(text, popupElements).then(function () {
-    showCopiedMessage(popupElements);
-  }, function (err) {
-    console.error('Error copying current url to clipboard: ', err);
-  });
-}
+  // Function to copy current url to clipboard.
+  // Shows a popupmessage on screen if url was copied successful.
+  Drupal.copyTextToClipboard = function (text, popupElements) {
+    if (!navigator.clipboard) {
+      Drupal.fallbackCopyTextToClipboard(text, popupElements);
+      return;
+    }
 
-/*
- * Fallback copy functionality using using older document.execCommand('copy') for when the normal clipboard
- * functionality (navigator.clipboard) does not work. This generates a textarea with url as content and the copies that
- * content using the document.execCommand('copy') command.
- */
-function fallbackCopyTextToClipboard(text, popupElements) {
-  'use strict';
-  var textArea = document.createElement('textarea');
-  textArea.value = text;
-  document.body.appendChild(textArea);
-  textArea.focus();
-  textArea.select();
+    navigator.clipboard.writeText(text, popupElements)
+      .then(function () {
+        Drupal.showCopiedMessage(popupElements);
+      }, function (err) {
+        console.error('Error copying current url to clipboard: ', err);
+      });
+  };
 
-  try {
-    document.execCommand('copy');
-    showCopiedMessage(popupElements);
-  }
-  catch (err) {
-    console.error('Error copying current url to clipboard', err);
-  }
+  // Fallback copy functionality using using older document.execCommand('copy') for when the normal clipboard
+  // functionality (navigator.clipboard) does not work. This generates a textarea with url as content and the copies that
+  // content using the document.execCommand('copy') command.
+  Drupal.fallbackCopyTextToClipboard = function (text, popupElements) {
 
-  document.body.removeChild(textArea);
-}
+    var $inputURL = $("<input>");
+    $("body").append($inputURL);
+    $inputURL.val(window.location.href).select();
 
-/*
- * Show a popup if the current url was successfully copied.
- */
-function showCopiedMessage(popupElements) {
-  'use strict';
-  var visibleClass = 'visible';
+    try {
+      document.execCommand("copy");
+      Drupal.showCopiedMessage(popupElements);
+    }
+    catch (err) {
+      console.error('Error copying current url to clipboard', err);
+    }
 
-  popupElements.forEach(function (element) {
-    element.classList.add(visibleClass);
-  });
+    $inputURL.remove();
+  };
 
-  setTimeout(function () {
-    popupElements.forEach(function (element) {
-      element.classList.remove(visibleClass);
+  // Show a popup if the current url was successfully copied.
+  Drupal.showCopiedMessage = function (popupElements) {
+    var visibleClass = 'visible',
+      $popupElements = popupElements;
+
+    $popupElements.each(function () {
+      $(this).addClass(visibleClass);
     });
-  }, 4000);
-}
+
+    setTimeout(function () {
+      $popupElements.each(function () {
+        $(this).removeClass(visibleClass);
+      });
+    }, 4000);
+  };
+
+})(jQuery, Drupal);
